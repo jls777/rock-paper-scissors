cca56f69d4d10368145ec1ac77c89b64314d94f3
John Sison <jlsison777@gmail.com>
2023-08-22 05:52:31 -0400

    added playRound function

--- rps.js
+++ rps.js
@@ -1,7 +1,25 @@
 function getComputerChoice() {
-  let compChoices = ['Rock', 'Paper', 'Scissors'];
+  let compChoices = ['rock', 'paper', 'scissors'];
   return compChoices[Math.floor(Math.random() * 3)];
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return 'Tie';
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') 
+  ) {
+    return 'Player wins';
+  } else {
+    return 'Computer wins';
+  }
+}
 
+const playerSelection = window.prompt().toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
 
